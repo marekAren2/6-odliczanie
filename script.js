4e8f6455d56027ee2019b8594f11bcd2d3f2dbe1
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-26 21:26:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let cyfra = 1;
+let boxMy = document.querySelector('.box');
+let cyfra = 11;
+boxMy.innerHTML = cyfra;
+console.log(boxMy);
+for (let i = 10; i > 0; i++) {
+  boxMy.innerHTML = i;
+}
